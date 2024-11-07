5944bf963f4e495e37b2e12b2767724a4b8e2c6f
chinelodedo <ericdobashi7@gmail.com>
2024-11-07 15:18:39 -0300

    PumpCan_studios.launcher
    A PumpCan studios é uma empresa destinada à criação e desenvolvimento de assets para jogos, responsável por arquiteturas e por modelagem, matrizes artísticas e gráficas. (projeto destinado a Interloper_SMP)

--- app/assets/js/distromanager.js
+++ app/assets/js/distromanager.js
@@ -3,9 +3,9 @@ const { DistributionAPI } = require('helios-core/common')
 const ConfigManager = require('./configmanager')
 
 // Old WesterosCraft url.
-// exports.REMOTE_DISTRO_URL = 'http://mc.westeroscraft.com/WesterosCraftLauncher/distribution.json'
-exports.REMOTE_DISTRO_URL = 'https://helios-files.geekcorner.eu.org/distribution.json'
-
+// exports.REMOTE_DISTRO_URL = 'https://interlopersmp.shop/nebula/distribution.json'
+exports.REMOTE_DISTRO_URL = 'https://interlopersmp.shop/nebula/distribution.json'
+n
 const api = new DistributionAPI(
     ConfigManager.getLauncherDirectory(),
     null, // Injected forcefully by the preloader.
